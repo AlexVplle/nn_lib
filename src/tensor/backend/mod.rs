c17d34afb6e76a2d29dc0c845a9d7cea714201b1
AlexVplle <48535782+AlexVplle@users.noreply.github.com>
2025-12-22 11:42:05 +0000

    add relu operation on CpuBackend

--- src/tensor/backend/mod.rs
+++ src/tensor/backend/mod.rs
@@ -9,5 +9,6 @@ use crate::error::NeuralNetworkError;
 
 pub trait Backend: Send + Sync {
     fn add(&self, lhs: &Arc<RwLock<Box<dyn StorageBackend>>>, rhs: &Arc<RwLock<Box<dyn StorageBackend>>>) -> Result<Box<dyn StorageBackend>, NeuralNetworkError>;
+    fn relu(&self, storage: &Arc<RwLock<Box<dyn StorageBackend>>>) -> Result<Box<dyn StorageBackend>, NeuralNetworkError>;
     fn device(&self) -> Device;
 }
